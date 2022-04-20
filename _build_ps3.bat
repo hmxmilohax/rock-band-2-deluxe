a70abd11f1f417db00134adf785e8b8bc1d92897
jnack <55568980+jnackmclain@users.noreply.github.com>
2022-04-19 20:57:57 -0500

    auto update and build rb2dx

---
+++ _build_ps3.bat
@@ -0,0 +1,2 @@
+git pull https://github.com/LlysiX/rock-band-2-deluxe develop
+"%~dp0dependencies/arkhelper" dir2ark "%~dp0\_ark" "%~dp0\_build\ps3\USRDIR\gen" -n "patch_ps3" -e -v 5
---
+++ _build_xbox.bat
@@ -0,0 +1,2 @@
+git pull https://github.com/LlysiX/rock-band-2-deluxe develop
+"%~dp0dependencies/arkhelper" dir2ark "%~dp0\_ark" "%~dp0\_build\xbox\gen" -n "patch_xbox" -e -v 5
