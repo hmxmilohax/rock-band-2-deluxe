f5260868d3c38da8bdf2016c89ca954cd6f42d70
jnack <55568980+jnackmclain@users.noreply.github.com>
2022-09-10 19:41:08 -0500

    update links

--- _build_xbox.bat
+++ _build_xbox.bat
@@ -1,3 +1,3 @@
-git pull https://github.com/LlysiX/rock-band-2-deluxe main
+git pull https://github.com/hmxmilohax/rock-band-2-deluxe main
 "%~dp0dependencies/arkhelper" dir2ark "%~dp0\_ark" "%~dp0\_build\xbox\gen" -n "patch_xbox" -e -v 5
 pause
--- _build_xbox.sh
+++ _build_xbox.sh
@@ -1,2 +1,2 @@
-git pull https://github.com/LlysiX/rock-band-2-deluxe main
+git pull https://github.com/hmxmilohax/rock-band-2-deluxe main
 dependencies/arkhelper dir2ark ./_ark ./_build/xbox/gen -n "patch_xbox" -e -v 5
