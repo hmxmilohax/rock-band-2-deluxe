ff7423bfeceb0c3cbd37ffc182e9db05ab714323
jnack <55568980+jnackmclain@users.noreply.github.com>
2022-12-31 11:55:03 -0600

    move opposite platform files on build

--- _build_ps3.sh
+++
@@ -1,2 +0,0 @@
-git pull https://github.com/hmxmilohax/rock-band-2-deluxe main
-dependencies/arkhelper dir2ark ./_ark ./_build/ps3/USRDIR/gen -n "patch_ps3" -e -v 5
--- _symlink_to_rpcs3.sh
+++
@@ -1,7 +0,0 @@
-if [ -d ~/.config/rpcs3/dev_hdd0/game/BLUS30147 ]; then
-	echo "~/.config/rpcs3/dev_hdd0/game/BLUS30147 already exists, not symlinking"
-else
-	mkdir -p ~/.config/rpcs3/dev_hdd0/game
-	ln -s "$PWD/_build/ps3" ~/.config/rpcs3/dev_hdd0/game/BLUS30147
-	echo "Symlinked _build/ps3/ to ~/.config/rpcs3/dev_hdd0/game/BLUS30147"
-fi
