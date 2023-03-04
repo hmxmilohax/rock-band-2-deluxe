5358ed6d2a02893ce5557f70627012249c99d03c
LysiX <smyler02@gmail.com>
2023-03-03 22:13:40 -0800

    rainbow od bar is back!

--- dependencies/dev_bats/_texture-process_gems.bat
+++
@@ -1,4 +0,0 @@
-cd "%~dp0custom_textures\gems"
-FOR /F "tokens=*" %%G IN ('dir /b *.png') DO "%~dp0dependencies/superfreq.exe" png2tex "%~dp0custom_textures\gems/%%G" "%~dp0%%~nG.png_xbox" --platform x360 --miloVersion 25
-cd "%~dp0"
-FOR /F "tokens=*" %%G IN ('dir /b *.png_xbox') DO python "%~dp0dependencies/swap_rb_art_bytes.py" "%~dp0%%G" "%~dp0%%~nG.png_ps3"
