b201e3dc46325befae2798a2c1712fdcc1f2ccae
LysiX <smyler02@gmail.com>
2023-03-12 16:28:34 -0700

    quick n dirty ps2 build script & workflow

--- user_scripts/build_ps2.py
+++ user_scripts/build_ps2.py
@@ -4,4 +4,4 @@ sys.path.append("../dependencies/dev_scripts")
 from build_ark import build_patch_ark
 
 build_patch_ark(False)
-print("You may find the files needed to place on your PS3 in /_build/PS3/.")
+print("You may find the files needed to create a PS2 ISO in /_build/PS2/.")
--- user_scripts/build_xbox.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.append("../dependencies/dev_scripts")
-from build_dx_settings_ark import build_dxsl_ark
-from build_ark import build_patch_ark
-
-if build_dxsl_ark():
-    if build_patch_ark(True):
-        print("You may find the files needed to place on your Xbox 360 in /_build/Xbox/.")
