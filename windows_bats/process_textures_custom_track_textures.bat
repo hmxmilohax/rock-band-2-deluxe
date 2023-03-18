83eaea920f3c15d69428e25e43ecd97669200132
LysiX <smyler02@gmail.com>
2023-03-18 00:32:56 -0700

    smashers

--- windows_bats/process_textures_custom_track_textures.bat
+++ windows_bats/process_textures_custom_track_textures.bat
@@ -1,5 +1,11 @@
+mkdir "%~dp0..\_tmp"
 cd "..\custom_textures/custom_track_textures"
 FOR /F "tokens=*" %%G IN ('dir /b *.png') DO "%~dp0..\dependencies/magick/magick.exe" convert "%~dp0..\custom_textures\custom_track_textures/%%G" "%~dp0..\custom_textures\custom_track_textures\%%~nG.png"
 FOR /F "tokens=*" %%G IN ('dir /b *.jpg') DO "%~dp0..\dependencies/magick/magick.exe" convert "%~dp0..\custom_textures\custom_track_textures/%%G" "%~dp0..\custom_textures\custom_track_textures\%%~nG.png"
 FOR /F "tokens=*" %%G IN ('dir /b *.bmp') DO "%~dp0..\dependencies/magick/magick.exe" convert "%~dp0..\custom_textures\custom_track_textures/%%G" "%~dp0..\custom_textures\custom_track_textures\%%~nG.png"
-FOR /F "tokens=*" %%G IN ('dir /b *.png') DO "%~dp0..\dependencies/superfreq.exe" png2tex "%~dp0..\custom_textures\custom_track_textures/%%G" "%~dp0..\_ark\track\custom_track_textures\gen\%%~nG.png_ps2"
+FOR /F "tokens=*" %%G IN ('dir /b *.png') DO "%~dp0..\dependencies/superfreq.exe" png2tex "%~dp0..\custom_textures\custom_track_textures/%%G" "%~dp0..\_tmp\%%~nG.bmp_ps2"
+cd "%~dp0..\_tmp"
+FOR /F "tokens=*" %%G IN ('dir /b *.bmp_ps2') DO "%~dp0..\dependencies/windows/superfreq.exe" tex2png "%~dp0..\_tmp/%%G" "%~dp0..\_tmp\%%~nG.png"
+FOR /F "tokens=*" %%G IN ('dir /b *.png') DO "%~dp0..\dependencies/windows/superfreq.exe" png2tex "%~dp0..\_tmp/%%G" "%~dp0..\_ark\track\custom_track_textures\gen\%%~nG.png_ps2"
+cd "%~dp0..\"
+rmdir "%~dp0..\_tmp" /s /q
