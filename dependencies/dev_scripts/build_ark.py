e591e6e1a76465243b923b6b699f440d16a27f13
LysiX <smyler02@gmail.com>
2023-10-03 21:05:24 -0700

    fix ps2 textures, ps2 building

--- dependencies/dev_scripts/build_ark.py
+++ dependencies/dev_scripts/build_ark.py
@@ -4,6 +4,8 @@ from subprocess import CalledProcessError
 from sys import platform
 import subprocess
 from check_git_updated import check_git_updated
+import shutil
+import stat
 import os
 
 def rm_tree(pth):
@@ -111,6 +113,106 @@ def build_patch_ark(xbox: bool, rpcs3_directory: str = None, rpcs3_mode: bool = 
     # remove temp directory
     if os.path.exists(root_dir.joinpath("_tmp")):
         rm_tree(root_dir.joinpath("_tmp"))
+
+    if not failed:
+        print("Successfully built Rock Band 2 Deluxe ARK.")
+        return True
+    else:
+        print("Error building ARK. Check your modifications or run git_reset.py to rebase your repo.")
+        return False
+
+def rmtree_harder(top):
+    for root, dirs, files in os.walk(top, topdown=False):
+        for name in files:
+            filename = os.path.join(root, name)
+            os.chmod(filename, stat.S_IWUSR)
+            os.remove(filename)
+        for name in dirs:
+            os.rmdir(os.path.join(root, name))
+    os.rmdir(top)
+
+#build ps2 ark
+def build_ps2_ark():
+    # directories used in this script
+    print("Building Rock Band 2 Deluxe patch arks...")
+    cwd = Path().absolute() # current working directory (dev_scripts)
+    root_dir = cwd.parents[0] # root directory of the repo
+    ark_dir = root_dir.joinpath("_ark")
+    ps2_ark_dir = root_dir.joinpath("_ark_ps2")
+    ps2_main_dir = root_dir.joinpath("_ps2_files")
+    tmpbuild_dir = root_dir.joinpath("_tmpbuild")
+
+    files_to_remove = "*_ps3"
+    ps2_files_to_remove = "*_xbox"
+    if platform == "win32":
+        build_location = "_build\ps2\\gen"
+    else:
+        build_location = "_build/ps2/gen"
+        # build the binaries if on linux/other OS
+        if platform != "darwin":
+            make_executable_binaries()
+    patch_hdr_version = "MAIN"
+
+    # pull the latest changes from the Rock Band 2 Deluxe repo if necessary
+    if not check_git_updated(repo_url="https://github.com/hmxmilohax/rock-band-2-deluxe", repo_root_path=root_dir):
+        cmd_pull = "git pull https://github.com/hmxmilohax/rock-band-2-deluxe main".split()
+        subprocess.run(cmd_pull, shell=(platform == "win32"), cwd="..")
+
+    # temporarily move other console's files out of the ark to reduce overall size
+    for f in ark_dir.rglob(files_to_remove):
+        temp_path = str(f).replace(f"{str(root_dir)}\\", "").replace(f"{str(root_dir)}/","")
+        # print(temp_path)
+        the_new_filename = root_dir.joinpath("_tmp").joinpath(temp_path)
+        the_new_filename.parent.mkdir(parents=True, exist_ok=True)
+        # print(f"moving file {temp_path} to {the_new_filename}")
+        f.rename(the_new_filename)
+
+    for f in ark_dir.rglob(ps2_files_to_remove):
+        temp_path = str(f).replace(f"{str(root_dir)}\\", "").replace(f"{str(root_dir)}/","")
+        # print(temp_path)
+        the_new_filename = root_dir.joinpath("_tmp").joinpath(temp_path)
+        the_new_filename.parent.mkdir(parents=True, exist_ok=True)
+        # print(f"moving file {temp_path} to {the_new_filename}")
+        f.rename(the_new_filename)
+
+    # copy all ark files to temp build folder
+    shutil.copytree(ark_dir, tmpbuild_dir, dirs_exist_ok=True)
+    shutil.copytree(ps2_ark_dir, tmpbuild_dir, dirs_exist_ok=True)
+    shutil.copytree(ps2_main_dir, tmpbuild_dir, dirs_exist_ok=True)
+    rmtree_harder(root_dir.joinpath("_tmpbuild/.git"))
+    os.remove(root_dir.joinpath("_tmpbuild/readme.md"))
+
+    # build the ark
+    failed = False
+    try:
+        if platform == "win32":
+            cmd_build = f"dependencies\windows\\arkhelper.exe dir2ark _tmpbuild {build_location} -n {patch_hdr_version} -e -v 4 -s 4073741823".split()
+        elif platform == "darwin":
+            cmd_build = f"dependencies/macos/arkhelper dir2ark _tmpbuild {build_location} -n {patch_hdr_version} -e -v 4 -s 4073741823".split()
+        else:
+            cmd_build = f"dependencies/linux/arkhelper dir2ark _tmpbuild {build_location} -n {patch_hdr_version} -e -v 4 -s 4073741823".split()
+        subprocess.check_output(cmd_build, shell=(platform == "win32"), cwd="..")
+    except CalledProcessError as e:
+        print(e.output)
+        failed = True
+
+    # move the other console's files back
+    for g in root_dir.joinpath("_tmp").rglob(files_to_remove):
+        final_path = str(g).replace(f"{str(root_dir)}\\_tmp\\", "").replace(f"{str(root_dir)}/_tmp/","")
+        # print(final_path)
+        g.rename(root_dir.joinpath(final_path))
+
+    for g in root_dir.joinpath("_tmp").rglob(ps2_files_to_remove):
+        final_path = str(g).replace(f"{str(root_dir)}\\_tmp\\", "").replace(f"{str(root_dir)}/_tmp/","")
+        # print(final_path)
+        g.rename(root_dir.joinpath(final_path))
+
+    # remove temp directory
+    if os.path.exists(root_dir.joinpath("_tmp")):
+        rm_tree(root_dir.joinpath("_tmp"))
+
+    #delete tmpbuild folder
+    rm_tree(tmpbuild_dir)
 
     if not failed:
         print("Successfully built Rock Band 2 Deluxe ARK.")
